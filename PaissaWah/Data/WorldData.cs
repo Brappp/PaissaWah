d19a018a0282fb66213f9cb30ffa897657ee2602
Wah <74884113+Brappp@users.noreply.github.com>
2024-08-25 18:29:34 -0400

    Update WorldData.cs

--- PaissaWah/Data/WorldData.cs
+++ PaissaWah/Data/WorldData.cs
@@ -15,14 +15,14 @@ namespace PaissaWah.Data
                 { "Dynamis", new List<string> { "Cuchulainn", "Golem", "Halicarnassus", "Kraken", "Maduin", "Marilith", "Rafflesia", "Seraph" }},
 
                 // EU
-                { "Chaos", new List<string> { "Cerberus", "Louisoix", "Moogle", "Omega", "Ragnarok", "Spriggan" }},
-                { "Light", new List<string> { "Lich", "Odin", "Phoenix", "Shiva", "Twintania", "Zodiark" }},
+                { "Chaos", new List<string> { "Cerberus", "Louisoix", "Moogle", "Omega", "Phantom", "Ragnarok","Sagittarius","Spriggan" }},
+                { "Light", new List<string> { "Alpha", "Lich", "Odin", "Phoenix", "Raiden", "Shiva", "Twintania", "Zodiark" }},
                 { "Shadow", new List<string> { "Innocence", "Pixie", "Titania", "Tycoon" }},
 
                 //JP
-                { "Elemental", new List<string> { "Aegis", "Atomos", "Carbuncle", "Garuda", "Gungnir", "Kujata", "Ramuh", "Tonberry", "Typhon", "Unicorn" }},
-                { "Gaia", new List<string> { "Alexander", "Bahamut", "Durandal", "Fenrir", "Ifrit", "Ridill", "Tiamat", "Ultima", "Valefor", "Yojimbo", "Zeromus" }},
-                { "Mana", new List<string> { "Anima", "Asura", "Chocobo", "Hades", "Ixion", "Mandragora", "Masamune", "Pandaemonium", "Shinryu", "Titan" }},
+                { "Elemental", new List<string> { "Aegis", "Atomos", "Carbuncle", "Garuda", "Gungnir", "Kujata", "Tonberry", "Typhon" }},
+                { "Gaia", new List<string> { "Alexander", "Bahamut", "Durandal", "Fenrir", "Ifrit", "Ridill", "Tiamat", "Ultima" }},
+                { "Mana", new List<string> { "Anima", "Asura", "Chocobo", "Hades", "Ixion", "Masamune", "Pandaemonium", "Titan" }},
                 { "Meteor", new List<string> { "Belias", "Mandragora", "Ramuh", "Shinryu", "Unicorn", "Valefor", "Yojimbo", "Zeromus" }},
 
                 //OCE
